382ff3dc2cd3b402474f0c923ab5e7c89a633078
saierukala <sai.erukala1508@gmail.com>
2023-01-07 10:32:03 +0000

    Not Founnd route

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+// Write your JS code here
+// Write your JS code here
+import React from 'react'
+
+const NotFound = () => (
+  <div>
+    <h1>Not Found</h1>
+  </div>
+)
+
+export default NotFound
